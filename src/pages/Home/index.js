c5edf1f30ed1508413a10ed4978e3b093ffebb18
ashidiqidimas <prabowo6@gmail.com>
2022-09-18 16:35:02 +0700

    finished home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,45 +2,38 @@ import React from "react";
 
 const Home = () => {
   return (
-    <main className="lg:container mx-auto text-center md:text-left">
-      <div className="flex flex-col md:flex-row h-screen pt-[76px]">
-        <div className="flex basis-1/5 md:basis-1/2 my-auto relative">
-          <img
-            src="/game-illustration.png"
-            alt="illustration of people playing game"
-            className="object-cover mx-auto my-auto"
-          />
-        </div>
-        <div className="flex basis-4/5 md:basis-1/2 justify-center items-center">
-          <div className="px-4">
-            <h1 className="text-3xl lg:text-4xl font-bold">Play Anything, Install Nothing.</h1>
-            <p className="text-xl mt-4 text-slate-500 w-full md:w-2/3">
-              To play, you don't have to install anything. Just
-              choose the game you want to play then you'll instantly be able to play it.</p>
-            <div className="flex gap-x-2 mt-4 justify-center md:justify-start">
-              <a
-                href="#"
-              >
-                <div
-                  className="bg-blue-500 hover:bg-blue-700 py-2 w-40 text-white font-semibold text-lg text-center rounded-lg"
+    <>
+      <main className="lg:container mx-auto text-center md:text-left">
+        <div className="flex flex-col md:flex-row h-screen pt-[76px]">
+          <div className="flex basis-1/5 md:basis-1/2 my-auto relative">
+            <img
+              src="/game-illustration.png"
+              alt="illustration of people playing game"
+              className="object-cover mx-auto my-auto"
+            />
+          </div>
+          <div className="flex basis-4/5 md:basis-1/2 justify-center items-center">
+            <div className="px-4">
+              <h1 className="text-3xl lg:text-4xl font-bold">Play Anything, Install Nothing.</h1>
+              <p className="text-xl mt-4 text-slate-500 w-full md:w-2/3">
+                To play, you don't have to install anything. Just
+                choose the game you want to play then you'll instantly be able to play it.</p>
+              <div className="flex gap-x-2 mt-4 justify-center md:justify-start">
+                <a
+                  href="#"
                 >
-                  Play Now
-                </div>
-              </a>
-              <a
-                href="#game-catalog"
-              >
-                <div
-                  className="border-2 border-blue-500 hover:bg-blue-100 py-1.5 w-40 text-blue-500 font-semibold text-lg text-center rounded-lg"
-                >
-                  Game Catalog
-                </div>
-              </a>
+                  <div
+                    className="bg-blue-500 hover:bg-blue-700 py-2 w-40 text-white font-semibold text-lg text-center rounded-lg"
+                  >
+                    Play Now
+                  </div>
+                </a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 };
 
