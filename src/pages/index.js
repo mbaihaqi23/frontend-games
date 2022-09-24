1de4931a9b2e5fd9952ba59ff75753e5353be829
mikaelkadga <mikaelkadga@gmail.com>
2022-09-24 17:42:54 +0700

    add cookies, styled-components, play-game

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Home from "./Home";
 import Register from "./Register";
 import Login from "./Login";
 import ProfilePage from "./ProfilePage";
-import DetailGame from "./DetailGame"
+import DetailGame from "./DetailGame";
+import PlayGame from "./PlayGame";
 
-export { Home, Register, Login, ProfilePage, DetailGame};
+export { Home, Register, Login, ProfilePage, DetailGame, PlayGame};
